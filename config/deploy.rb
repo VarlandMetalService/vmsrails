2b1b3df99d53879c8c68c091f9302ad543a32057
Toby Varland <toby@tobyvarland.com>
2018-05-05 21:21:46 -0400

    Whenever gem update

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,3 +42,6 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# Whenever gem configuration for Capistrano
+set :whenever_roles, -> {[:db, :app]}
