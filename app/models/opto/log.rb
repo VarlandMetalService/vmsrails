7d96cfebb488331590eac9f74e8dbfb8eca19755
Toby Varland <toby@tobyvarland.com>
2018-05-23 11:51:48 -0400

    Fix time zone bug

--- app/models/opto/log.rb
+++ app/models/opto/log.rb
@@ -27,8 +27,7 @@ class Opto::Log < ApplicationRecord
   # Instance methods.
 
   def parse_controller_timestamp(raw)
-    raw_timestamp = ::DateTime.strptime(raw, "%m/%d/%Y %H:%M:%S")
-    self.controller_timestamp = ::ActiveSupport::TimeZone['Eastern Time (US & Canada)'].parse(raw_timestamp.strftime('%Y-%m-%d %H:%M:%S'))
+    self.controller_timestamp = ::DateTime.strptime(raw, "%m/%d/%Y %H:%M:%S")
   end
 
   def log_type
