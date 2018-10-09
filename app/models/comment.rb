2ca2ec04efc13ab0a8adc8b4866560352de85544
Richard Legacy <richard.legacy@varland.com>
2018-10-09 16:40:07 -0400

    [LAYOUT] Makes print jobs look decent on mobile.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
     belongs_to :user, class_name: '::User', foreign_key: 'user_id', optional: true
 
     mount_uploader :attachment, AttachmentUploader
+
+    validates_presence_of :body
 end
