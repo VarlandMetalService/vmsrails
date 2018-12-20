331788230d44bd1a56869252100caeefcd93d849
Richard Legacy <richard.legacy@varland.com>
2018-12-20 12:50:09 -0500

    typo

--- app/controllers/printing/print_jobs_controller.rb
+++ app/controllers/printing/print_jobs_controller.rb
@@ -7,7 +7,7 @@ class Printing::PrintJobsController < ApplicationController
   require "base64"
   require "uri"
   require "net/http"
-  
+
   has_scope :with_user
   has_scope :with_doc_type
   has_scope :with_workstation
@@ -34,7 +34,7 @@ class Printing::PrintJobsController < ApplicationController
     data = Printing::PrintJob.parse_data(params[:data])
     @print_job = Printing::PrintJob.new(data)
     out_params = {'data' => params[:data] }
-      x = Net::HTTP.post_form(URI.parse('192.168.100.7:3000/printing/print_jobs'), params)
+      x = Net::HTTP.post_form(URI.parse('192.168.100.7:3000/printing/print_jobs'), out_params)
     respond_to do |format|
       if @print_job.save
         Printing::PrintJob.set_queue(@print_job)
