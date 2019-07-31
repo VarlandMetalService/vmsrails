d5e3d5ae4b3ddf453ea71037637bf9a8bbb1fa37
Toby Varland <toby.varland@varland.com>
2019-07-31 15:32:19 +0000

    Update monthly high kwh message

--- app/models/opto/monthly_high_kwh.rb
+++ app/models/opto/monthly_high_kwh.rb
@@ -5,7 +5,11 @@ class Opto::MonthlyHighKwh < Opto::Log
   # Instance methods.
 
   def details
-    "New KWH high for month: <strong><code>#{::ActiveSupport::NumberHelper::number_to_delimited self.reading}</code></strong>."
+    "New KWH high for month: <strong><code>#{::ActiveSupport::NumberHelper::number_to_delimited self.reading} KWH</code></strong>."
+  end
+
+  def log_type
+    "Monthly High KWH"
   end
 
   def notification_settings
