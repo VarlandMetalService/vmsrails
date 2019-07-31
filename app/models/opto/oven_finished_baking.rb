716358b78f11ea5bfb5b311638b8809e463be6f9
Toby Varland <toby.varland@varland.com>
2019-07-31 21:20:36 +0000

    Fix log detail formatting.

--- app/models/opto/oven_finished_baking.rb
+++ app/models/opto/oven_finished_baking.rb
@@ -5,7 +5,7 @@ class Opto::OvenFinishedBaking < Opto::Log
   # Instance methods.
 
   def details
-    "Oven finished baking. Oven: <strong><code>#{self.oven}#{self.side}</code></strong>"
+    "Oven finished baking. Oven: <strong><code>#{self.oven}#{self.side}</code></strong>."
   end
 
   def notification_settings
