f69b24ebf27cfabd8ac18a9f53c25fd58af284bb
Toby Varland <toby.varland@varland.com>
2019-10-17 18:06:53 +0000

    Update clarifier warning messages

---
+++ app/models/opto/clarifier_slope_upset.rb
@@ -0,0 +1,19 @@
+require 'json'
+
+class Opto::ClarifierSlopeUpset < Opto::Log
+
+  # Instance methods.
+
+  def details
+    "Clarifier slope upset started. Limit: #{self.limit.to_f.round(2)}. Reading: #{self.reading.to_f.round(2)}."
+  end
+
+  def notification_settings
+    {
+      enabled: true,
+      subject: "Clarifier: Slope Upset",
+      recipients: [Opto::FOREMEN_EMAIL, Opto::BRIAN_EMAIL, Opto::TOBY_EMAIL, Opto::RICH_EMAIL, Opto::BRIAN_CELL, Opto::TOBY_CELL, Opto::RICH_CELL]
+    }
+  end
+
+end
