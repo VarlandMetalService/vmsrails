8a9d8852934b78ce8ad200566c5209621468d1df
Toby Varland <toby.varland@varland.com>
2021-01-11 15:21:42 -0500

    Fix bug in Opto mailer

--- app/mailers/opto_mailer.rb
+++ app/mailers/opto_mailer.rb
@@ -6,15 +6,17 @@ class OptoMailer < ApplicationMailer
 
   def opto_notification
     @log = params[:log]
-    if @log.notification_settings[:recipients].include?(Opto::FOREMEN_EMAIL)
-      @log.notification_settings[:recipients].delete(Opto::FOREMEN_EMAIL)
+    if @log.notification_settings[:recipients].include?("toby.varland@varland.com")
+      @log.notification_settings[:recipients].delete("toby.varland@varland.com")
       uri = "http://timeclock.varland.com/foremen_email.json"
       response = Net::HTTP.get(uri)
       addresses = JSON.parse(response)
-      @addresses.each do |email|
+      addresses.each do |email|
         @log.notification_settings[:recipients] << email
       end
     end
+    puts @log
+    return
     timestamp = Time.now.strftime("%m/%d/%y %l:%M%P")
     mail(to: @log.notification_settings[:recipients],
          subject: "#{@log.notification_settings[:subject]} (#{timestamp})")
