a25b125e64aa8107bf2fa3706914b2035b3f9f34
Toby Varland <toby.varland@varland.com>
2021-03-01 14:26:14 -0500

    Add log definition for EN amp warning

---
+++ app/models/opto/en_amp_warning.rb
@@ -0,0 +1,19 @@
+require 'json'
+
+class Opto::EnAmpWarning < Opto::Log
+
+  # Instance methods.
+
+  def notification_settings
+    {
+      enabled: true,
+      subject: "Dept. 3: EN Amp Warning",
+      recipients: ["brian.mangold@varland.com", "mike.mitchell.jr@varland.com", "joel.perrine@varland.com"]
+    }
+  end
+
+  def details
+    "EN amp warning. Limit: <strong><code>#{self.limit.to_f.round(3)} amps</code></strong>. Reading: <strong><code>#{self.reading.to_f.round(3)} amps</code></strong>."
+  end
+
+end
